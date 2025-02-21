6594b4a00d5c297472b9985002b1751de9a7b270
Nguyễn Tiến Mạnh <topallaivn@gmail.com>
2025-02-22 05:27:32 +0700

    update front-end

--- vite-projectasdasdasdkassdkasksd/src/Pushing_Information.jsx
+++ vite-projectasdasdasdkassdkasksd/src/Pushing_Information.jsx
@@ -15,7 +15,20 @@ function Pushing_Information() {
         leg_length,
         bmi
       };
-      
+      fetch('https://localhost:3000/data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(result => {
+        console.log('Success:', result);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
       console.log(data);
     }
     
